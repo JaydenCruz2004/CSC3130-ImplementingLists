0a06bac8b57d6becfd5e8027aff4fc9ee737e50d
Jayden <jaydencruz824@gmail.com>
2025-02-13 18:25:47 -0600

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Song song = new Song("El Pibe De Mi Barrio", "Dr. Krapula", 2.47);
-        Song one = new Song("505", "Artic Monkeys", 4.14);
-        System.out.println(song.toString());
-        System.out.println(one.toString());
+        //Song song = new Song("El Pibe De Mi Barrio", "Dr. Krapula", 2.47);
+        //Song one = new Song("505", "Artic Monkeys", 4.14);
+        //System.out.println(song.toString());
+        //System.out.println(one.toString());
 
         //System.out.println("songName: "  +song.getTitle() + ", artist: " + song.getArtist() + ", length: " + song.getLength());
         //System.out.println("songName: " + one.getTitle() + ", artist: " + one.getArtist() + ", length: " + one.getLength());

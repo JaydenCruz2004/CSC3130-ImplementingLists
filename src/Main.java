4f4954e264a288da3d42bf20c343f74ceda87e39
Jayden <jaydencruz824@gmail.com>
2025-02-15 17:54:43 -0600

    implements main method for the 3 list classes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        //Song song = new Song("El Pibe De Mi Barrio", "Dr. Krapula", 2.47);
-        //Song one = new Song("505", "Artic Monkeys", 4.14);
-        //System.out.println(song.toString());
-        //System.out.println(one.toString());
+        // Test Array List (AList)
+        AList<Song> aList = new AList<>();
+        System.out.println("Empty AList: " + aList);
 
-        //System.out.println("songName: "  +song.getTitle() + ", artist: " + song.getArtist() + ", length: " + song.getLength());
-        //System.out.println("songName: " + one.getTitle() + ", artist: " + one.getArtist() + ", length: " + one.getLength());
-        //System.out.println("Hello, World!");
+        aList.addy(new Song("El Pibe De Mi Barrio", "Dr. Krapula", 2.47));
+        aList.addy(new Song("505", "Arctic Monkeys", 4.14));
+        aList.addy(new Song("NADIE", "Tito Double P", 3.17));
+        aList.addy(new Song("DtMF", "Bad Bunny", 3.57));
+
+        System.out.println("Songs AList:");
+        System.out.println(aList);
+
+        aList.removy(3);
+        aList.removy(1);
+        System.out.println("Songs AList after removing at indices 3 and 1:");
+        System.out.println(aList);
+
+        // Test Singly Linked List (SLList)
+        SLList<Song> slList = new SLList<>();
+        System.out.println("Empty SLList: " + slList);
+
+        slList.addy(new Song("El Pibe De Mi Barrio", "Dr. Krapula", 2.47));
+        slList.addy(new Song("505", "Arctic Monkeys", 4.14));
+        slList.addy(new Song("NADIE", "Tito Double P", 3.17));
+        slList.addy(new Song("DtMF", "Bad Bunny", 3.57));
+
+        System.out.println("Songs SLList:");
+        System.out.println(slList);
+
+        slList.removy(3);
+        slList.removy(1);
+        System.out.println("Songs SLList after removing at indices 3 and 1:");
+        System.out.println(slList);
+
+        // Test Doubly Linked List (DLList)
+        DLList<Song> dlList = new DLList<>();
+        System.out.println("Empty DLList: " + dlList);
+
+        dlList.addy(new Song("El Pibe De Mi Barrio", "Dr. Krapula", 2.47));
+        dlList.addy(new Song("505", "Arctic Monkeys", 4.14));
+        dlList.addy(new Song("NADIE", "Tito Double P", 3.17));
+        dlList.addy(new Song("DtMF", "Bad Bunny", 3.57));
+
+        System.out.println("Songs DLList:");
+        System.out.println(dlList);
+
+        dlList.removy(3);
+        dlList.removy(1);
+        System.out.println("Songs DLList after removing at indices 3 and 1:");
+        System.out.println(dlList);
     }
 }
